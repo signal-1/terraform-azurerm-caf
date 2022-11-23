60cc7c27d86ce84b4a113f85774154a9e2e96132
Oleksandr Trapeznikov <oleksandr.trapeznikov@gmail.com>
2022-11-23 18:51:37 +0000

    postresql kv in different lz

--- postgresql_servers.tf
+++ postgresql_servers.tf
@@ -17,7 +17,7 @@ module "postgresql_servers" {
   resource_group_name = can(each.value.resource_group.name) || can(each.value.resource_group_name) ? try(each.value.resource_group.name, each.value.resource_group_name) : local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group_key, each.value.resource_group.key)].name
   base_tags           = try(local.global_settings.inherit_tags, false) ? local.combined_objects_resource_groups[try(each.value.resource_group.lz_key, local.client_config.landingzone_key)][try(each.value.resource_group.key, each.value.resource_group_key)].tags : {}
 
-  keyvault_id       = try(each.value.administrator_login_password, null) == null ? module.keyvaults[each.value.keyvault_key].id : null
+  keyvault_id       = can(each.value.administrator_login_password) ? null : local.combined_objects_keyvaults[try(each.value.keyvault.lz_key, local.client_config.landingzone_key)][try(each.value.keyvault_key, each.value.keyvault.key)].id
   storage_accounts  = module.storage_accounts
   azuread_groups    = module.azuread_groups
   vnets             = local.combined_objects_networking
